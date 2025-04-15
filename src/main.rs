83846dc8bf929e7fa04119d2cf3f7186ace887af
SATHVIK SVS <svssathvik77@gmail.com>
2025-04-15 10:45:41 +0530

    init axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::IntoResponse, routing::get, Router};
+use tokio::net::TcpListener;
+
+async fn greet() -> impl IntoResponse {
+    "Welcome to garden virtual balances"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(greet));
+    let addr = "0.0.0.0:3001";
+    let tcp_listener = TcpListener::bind(&addr).await.unwrap();
+    println!("Listening on {}", &addr);
+    axum::serve(tcp_listener, app).await.unwrap();
 }
